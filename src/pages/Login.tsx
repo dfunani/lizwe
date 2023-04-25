58eb603a35a3252bd69df53365afd7c6a350bb54
DelaliFunani <dfunani@live.co.za>
2023-04-25 15:03:35 +0200

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,7 +19,7 @@ export default function Login(){
 <div className={"tab-content " + styles.login} id="myTabContent">
 
   {/* Tab Info   */}
-  <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabIndex="0">
+  <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabIndex={0}>
   <div className={"card mb-3 " + styles.cards}>
   <div className="row g-0">
     <div className="col-md-4">
@@ -41,7 +41,7 @@ export default function Login(){
   </div>
   
   {/* Tab Info   */}
-  <div className="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabIndex="0">
+  <div className="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabIndex={0}>
   <div className={"card mb-3 " + styles.cards}>
   <div className="row g-0">
     <div className="col-md-4">
